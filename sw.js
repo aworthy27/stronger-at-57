55572bf62b75ff901a2d33b24cba80647f60846e
aworthy27 <148841498+aworthy27@users.noreply.github.com>
2026-09-17 11:29:46 -0700

    Refresh PWA cache for new exercise artwork

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='stronger57-v6';
+const CACHE='stronger57-v7';
 const ASSETS=['./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
